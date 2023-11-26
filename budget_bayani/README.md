8a0047eb2a44f2f59320792e5e9c66dab61c3343
spadrejuan <shaunpadrejuan@gmail.com>
2023-11-26 14:22:34 +0800

    Added flutter dependencies and base files to jumpstart development

---
+++ budget_bayani/README.md
@@ -0,0 +1,3 @@
+# budget_bayani
+
+A finance app made in compliance with the Final Academic Project for ICS26011
---
+++ budget_bayani/android/app/src/main/kotlin/com/budget_bayani/budget_bayani/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.budget_bayani.budget_bayani
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
