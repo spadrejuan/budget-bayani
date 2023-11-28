04cacc9c9fe94a70d9cf16267edf3186308844fa
spadrejuan <shaunpadrejuan@gmail.com>
2023-11-29 00:56:56 +0800

    Added splash screens and logo assets for app

--- budget_bayani/lib/main.dart
+++ budget_bayani/lib/main.dart
@@ -8,8 +8,8 @@ class BudgetBayani extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
-        primaryColor: Color(0xFF0A0E21),
-        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        primaryColor: const Color(0xFF0A0E21),
+        scaffoldBackgroundColor: const Color(0xFF0A0E21),
       ),
       home: LandingPage(),
     );
