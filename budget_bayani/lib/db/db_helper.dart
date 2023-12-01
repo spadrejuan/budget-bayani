25f210b69308d86ff4857eed337fd72b60815997
spadrejuan <shaunpadrejuan@gmail.com>
2023-12-01 16:21:54 +0800

    Added working code for displaying in database

--- budget_bayani/lib/db/db_helper.dart
+++ budget_bayani/lib/db/db_helper.dart
@@ -1,75 +1,58 @@
 import 'dart:async';
-import 'dart:async';
-
 import 'package:budget_bayani/models/limits.dart';
 import 'package:budget_bayani/models/logs.dart';
 import 'package:flutter/material.dart';
 import 'package:sqflite/sqflite.dart';
-import 'dart:async';
 import '../models/goals.dart';
-
+import 'package:path/path.dart';
 class DBHelper {
-  static Future _onConfigure(Database database) async {
+  static final DBHelper _databaseHelper = DBHelper._();
+  DBHelper._();
+  late Database db;
+  factory DBHelper() {
+    return _databaseHelper;
+  }
+  Future _onConfigure(Database database) async {
     await database.execute('PRAGMA foreign_keys = ON');
   }
 
-  static Future<void> createTables(Database database) async {
-    await database.execute("CREATE TABLE incomes(income_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"
+  Future<void> createTables(Database database) async {
+    await database.execute("CREATE TABLE IF NOT EXISTS incomes(income_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"
         "income_date DATETIME, income_amount DOUBLE,"
         "income_category TEXT, income_note TEXT)");
-    await database.execute("CREATE TABLE expenses(expense_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"
+    await database.execute("CREATE TABLE IF NOT EXISTS expenses(expense_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"
         "expense_date DATETIME, expense_amount DOUBLE,"
         "expense_category TEXT, expense_note TEXT)");
-    await database.execute("CREATE TABLE goals(goal_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"
-        "goal_name TEXT, goal_start DATETIME,"
-        "goal_end DATETIME, goal_amount DOUBLE, income_category TEXT)");
-    await database.execute("CREATE TABLE limits(limit_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"
+    await database.execute("CREATE TABLE IF NOT EXISTS goals(goal_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"
+        "goal_name TEXT NOT NULL, goal_start TEXT NOT NULL,"
+        "goal_end TEXT NOT NULL, goal_amount DOUBLE NOT NULL, income_category TEXT NOT NULL)");
+    await database.execute("CREATE TABLE IF NOT EXISTS  limits(limit_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"
         "limit_number DOUBLE)");
   }
-
-  static Future<Database> db() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    return openDatabase('budget_bayani.db',
-      version: 1,
-      onConfigure: _onConfigure,
-      onCreate: (Database database, int version) async {
+  Future<void> initDB() async {
+    String path = await getDatabasesPath();
+    db = await openDatabase(
+      join(path, 'budget_bayani.db'),
+      onCreate: (database, version) async {
         await createTables(database);
       },
+      version: 1,
     );
   }
-
-  static Future<int> createGoal(Goal goal) async {
-    final db = await DBHelper.db();
-    final data = goal.toMap();
-    final id = await db.insert(
-        'goals', data, conflictAlgorithm: ConflictAlgorithm.replace);
-    return id;
-  }
-
-  static Future<List<Map<String, dynamic>>> getGoals() async {
-    final db = await DBHelper.db();
-    return db.query('goals', orderBy: 'goal_id');
-  }
-
-  // To get a specific instance
-  // static Future<List<Map<String, dynamic>>> getGoal(Goal goal) async{
-  //   final db = await DBHelper.db();
-  //   return db.query('goals', where: "goal_id=?", whereArgs: [goal.goalId], limit: 1);
-  // }
-  static Future<int> updateGoal(int id, Goal goal) async {
-    final db = await DBHelper.db();
-    final data = goal.toMap();
-    final result = await db.update(
-        'goals', data, where: "goal_id=?", whereArgs: [id]);
+  Future<int> insertGoal(Goal goal) async {
+    int result = await db.insert('goals', goal.toMap());
     return result;
   }
-
-  static Future<void> deleteGoal(int id, Goal goal) async {
-    final db = await DBHelper.db();
-    try {
-      await db.delete('goals', where: 'goal_id=?', whereArgs: [id]);
-    } catch (err) {
-      debugPrint("Something went wrong: $err");
-    }
+  Future<int> updateGoal(Goal goal) async {
+    int result = await db.update('goals', goal.toMap(), where: 'goal_id=?', whereArgs: [goal.goalId],);
+    return result;
   }
+  Future<List<Goal>> retrieveGoals() async {
+    final List<Map<String, Object?>> queryResult = await db.query('goals');
+    return queryResult.map((e) => Goal.fromMap(e)).toList();
+  }
+  Future<void> deleteUser(int id) async {
+    await db.delete('goals', where: 'goal_id=?', whereArgs: [id]);
+  }
+
 }
--- budget_bayani/lib/models/goals.dart
+++ budget_bayani/lib/models/goals.dart
@@ -1,54 +1,62 @@
 import 'db_objects.dart';
 // Inheritance, Encapsulation
-class Goal extends dbObject{
- // int _goalId;
+class Goal extends dbObject {
+  int? goalId;
   String _goalName;
   String _goalStart;
   String _goalEnd;
   double _goalAmount;
   String _incomeCategory;
+
   Goal({
-  //  required int goalId,
+    this.goalId,
     required String goalName,
     required String goalStart,
     required String goalEnd,
     required double goalAmount,
     required String incomeCategory,
-  }): /*_goalId = goalId,*/ _goalName = goalName, _goalStart = goalStart, _goalEnd = goalEnd, _goalAmount = goalAmount,
+  })
+      : _goalName = goalName,
+        _goalStart = goalStart,
+        _goalEnd = goalEnd,
+        _goalAmount = goalAmount,
         _incomeCategory = incomeCategory;
-  // int get goalId => _goalId!;
-  // set goalId(int value) {
-  //   _goalId = value;
-  // }
 
-  String get goalName => _goalName!;
+
+  String get goalName => _goalName;
+
   set goalName(String value) {
     _goalName = value;
   }
 
-  String get goalStart => _goalStart!;
+  String get goalStart => _goalStart;
+
   set goalStart(String value) {
     _goalStart = value;
   }
 
-  String get goalEnd => _goalEnd!;
+  String get goalEnd => _goalEnd;
+
   set goalEnd(String value) {
     _goalEnd = value;
   }
 
-  double get goalAmount => _goalAmount!;
+  double get goalAmount => _goalAmount;
+
   set goalAmount(double value) {
     _goalAmount = value;
   }
-  String get incomeCategory => _incomeCategory!;
+
+  String get incomeCategory => _incomeCategory;
+
   set incomeCategory(String value) {
     _incomeCategory = value;
   }
 
   @override
-  Map<String, dynamic> toMap(){
-    return{
-     // 'goalId': goalId,
+  Map<String, Object?> toMap() {
+    return {
+      'goal_id': goalId,
       'goal_name': goalName,
       'goal_start': goalStart,
       'goal_end': goalEnd,
@@ -56,11 +64,19 @@ class Goal extends dbObject{
       'income_category': incomeCategory
     };
   }
+  Goal.fromMap(Map <String, dynamic> map)
+    :
+      goalId = map["goal_id"],
+      _goalName = map["goal_name"],
+      _goalStart = map["goal_start"],
+      _goalEnd = map["goal_end"],
+      _goalAmount = map["goal_amount"],
+      _incomeCategory = map["income_category"];
+
   @override
-  String toString(){
-    // return 'Goal{key: $goalId, name: $goalName, start: $goalStart, end: $goalEnd'
-    //     'goalAmount: $goalAmount, incomeCategory: $incomeCategory}';
-    return 'Goal{name: $goalName, start: $goalStart, end: $goalEnd'
+  String toString() {
+    return 'Goal{key: $goalId, name: $goalName, start: $goalStart, end: $goalEnd'
         'goalAmount: $goalAmount, incomeCategory: $incomeCategory}';
   }
+
 }
